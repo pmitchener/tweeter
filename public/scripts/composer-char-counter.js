982a63e665a9f583ca1861f2c7fda5146e41a661
Patrick Mitchener <pmitchener@rogers.com>
2020-08-12 01:27:39 +0000

    All files for front end first phase.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+const maxCharCount = 140;//limit of charactero count in tweet input.
+$(document).ready(() => {
+  $("#tweet-char-count").val(maxCharCount);
+  $("#tweet-text").keyup(function() {
+    const currentCount = this.value.length;
+    const objCounter = $(this).parent().children('div').children('.counter');
+    objCounter.val(maxCharCount - currentCount);
+    if (currentCount > maxCharCount) {
+      objCounter.addClass('counter-over-flow');
+    } else {
+      objCounter.removeClass('counter-over-flow');
+    }
+  });
+});
