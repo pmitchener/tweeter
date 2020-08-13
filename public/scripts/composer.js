cbc96667985ed918478a9daf678f3b0421efdfaf
Patrick Mitchener <pmitchener@rogers.com>
2020-08-13 16:36:37 +0000

    Added tweeter form toggle and scroll to top button

---
+++ public/scripts/composer.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  loadTweets();
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 100) {
+      $(".scrollUpButton").removeClass("hide-scrollUpButton");
+    }
+  });
+  $(".scrollUpButton").click( function(evt) {
+    $(window).scrollTop(0);
+    $(this).addClass("hide-scrollUpButton");
+  })
+  $(".new-tweet-button").click(function() {
+    $(".new-tweet").slideToggle();
+  });
+  $("new-tweet-down-arrow").animate({"top": "-=130px"}, 4000, "linear") ;
+  $("new-tweet-down-arrow").animate({"top": "+=130px"}, 4000, "linear") ;
+
+});
